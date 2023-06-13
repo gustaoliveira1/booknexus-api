4b04916de6d196ec8d2cec25007ea83fc2cd069d
Gusta Oliveira <gusta01.oliveira@gmail.com>
2023-06-13 11:02:38 -0300

    chore: create Book.spec.ts

---
+++ spec/models/Book.spec.ts
@@ -0,0 +1,56 @@
+import { describe, test, expect, jest } from '@jest/globals'
+import { Book } from '../../src/models/Book'
+
+describe('Book model', () => {
+  test('should create a book with valid properties', () => {
+    const book = new Book({
+      id: '01',
+      title: 'Pandas are cute',
+      author: 'John Doe',
+      genre: 'Comedy',
+      publicationYear: 2004,
+      sinopse: 'I Dont Know'
+    })
+
+    expect(book.id).toBe('01')
+    expect(book.title).toBe('Pandas are cute')
+    expect(book.author).toBe('John Doe')
+    expect(book.genre).toBe('Comedy')
+    expect(book.publicationYear).toBe(2004)
+    expect(book.sinopse).toBe('I Dont Know')
+  })
+
+  test('should generate and id for an new book', () => {
+    const book = new Book({
+      title: 'Pandas are cute',
+      author: 'John Doe',
+      genre: 'Comedy',
+      publicationYear: 2004,
+      sinopse: 'I Dont Know',
+    })
+
+    expect(book.id).toBeDefined()
+    expect(book.title).toBe('Pandas are cute')
+    expect(book.author).toBe('John Doe')
+    expect(book.genre).toBe('Comedy')
+    expect(book.publicationYear).toBe(2004)
+    expect(book.sinopse).toBe('I Dont Know')
+  })
+
+  test('should call "create" method when creating a new book', async () => {
+    const book = new Book({
+      title: 'Pandas are cute',
+      author: 'John Doe',
+      genre: 'Comedy',
+      publicationYear: 2004,
+      sinopse: 'I Dont Know',
+    })
+
+    const mockCreateFunction = jest.fn(async () => { return })
+
+    book.create = mockCreateFunction
+    book.create()
+
+    expect(mockCreateFunction).toHaveBeenCalled()
+  })
+})
