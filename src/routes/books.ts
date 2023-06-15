dea36b1b1bfb644082d43c720b3f7cdc83967855
Gusta Oliveira <gusta01.oliveira@gmail.com>
2023-06-15 10:45:43 -0300

    feat: create book endpoints

---
+++ src/routes/books.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from 'fastify'
+import { CreateBookController } from '../controllers/book/Create'
+import { UpdateBookController } from '../controllers/book/Update'
+import { GetBookController } from '../controllers/book/Get'
+import { RemoveBookController } from '../controllers/book/Remove'
+
+export const bookRoutes = async (app: FastifyInstance) => {
+  app.get('/books', async (request) => {
+    const getBookController = new GetBookController(request)
+    const books = request.query
+      ? await getBookController.findByName()
+      : await getBookController.getAll()
+
+    return books
+  })
+
+  app.get('/books/:id', async (request, response) => {
+    const getBookController = new GetBookController(request)
+    const book = await getBookController.findById()
+
+    if (!book) {
+      return response.send(404)
+    }
+
+    return book
+  })
+
+  app.post('/books', async (request) => {
+    const createBookController = new CreateBookController(request)
+    const book = await createBookController.handle()
+
+    return book
+  })
+
+  app.put('/books/:id', async (request) => {
+    const updateBookController = new UpdateBookController(request)
+    const book = await updateBookController.handle()
+
+    return book
+  })
+
+  app.delete('/books/:id', async (request) => {
+    const removeBookController = new RemoveBookController(request)
+    await removeBookController.handle()
+  })
+}
